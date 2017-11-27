a03051b17f6377b163055e8c775988faa7022b4e
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-11-27 11:59:55 +0000

    .

--- src/main/java/com/librarygif/controller/GifController.java
+++ src/main/java/com/librarygif/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.librarygif.controller;
 
+import com.librarygif.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -12,15 +16,16 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping (value = "/gif")
-    @ResponseBody
-    public String listGif(){
-        return "The Gif !!";
-    }
-
     @RequestMapping(value = "/index")
     @ResponseBody
     public String home() {
         return "Hello, Contact Manager!";
+    }
+
+    @RequestMapping(value = "/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif ("compiler-bot", LocalDate.of(2017,10, 4), "Antonio", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
