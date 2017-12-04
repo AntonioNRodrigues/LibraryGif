ea7a279323a6a06825f104550df841647644526d
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-12-04 23:07:47 +0100

    new features

--- src/main/java/com/librarygif/controller/GifController.java
+++ src/main/java/com/librarygif/controller/GifController.java
@@ -5,10 +5,12 @@ import com.librarygif.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
@@ -16,7 +18,9 @@ public class GifController {
     private GifRepository gifRepository;
 
     @RequestMapping ("/")
-    public String listGifs(){
+    public String listGifs(ModelMap modelMap){
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
@@ -26,9 +30,9 @@ public class GifController {
         return "Hello, Contact Manager!";
     }
 
-    @RequestMapping(value = "/gif")
-    public String gifDetails(ModelMap modelMap){
-        Gif gif = gifRepository.findByName("android-explosion");
+    @RequestMapping(value = "/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
--- src/main/java/com/librarygif/data/GifRepository.java
+++ src/main/java/com/librarygif/data/GifRepository.java
@@ -27,4 +27,8 @@ public class GifRepository {
         }
         return  null;
     }
+
+    public List<Gif> getAllGifs(){
+        return ALL_GIFS;
+    }
 }
